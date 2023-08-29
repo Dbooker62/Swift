b752e1085da2f6fbbbb7af59040433108b291c30
Dbooker62 <123719363+Dbooker62@users.noreply.github.com>
2023-08-29 00:39:18 -0500

    Create MenuDanielB.swift

---
+++ MenuDanielB.swift
@@ -0,0 +1,20 @@
+let appetizer = "Caesar Salad"
+let appetizerPrice = 7.99
+
+let mainCourse = "Grilled Salmon"
+let mainCoursePrice = 15.99
+
+let dessert = "Chocolate Fondue"
+let dessertPrice = 8.49
+
+
+var specialOfTheDay = "Pasta Primavera"
+var specialOfTheDayPrice = 12.99
+
+
+print("Welcome to Rib House!")
+print("Menu:")
+print("1. \(appetizer) - $\(appetizerPrice)")
+print("2. \(mainCourse) - $\(mainCoursePrice)")
+print("3. \(dessert) - $\(dessertPrice)")
+print("4. Today's Special: \(specialOfTheDay) - $\(specialOfTheDayPrice)")
